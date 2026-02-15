287ecbbd233ffd93ed519977b8ae05f2b47faf95
starwald <mal25717@uvg.edu.gt>
2026-02-15 17:19:20 -0600

    Implementación del Enlazado Simple para la Lista

---
+++ SinglyLinkedList.java
@@ -0,0 +1,39 @@
+package HojaDeTrabajo4;
+
+import java.util.ArrayList;
+
+public class SinglyLinkedList<T> implements List<T> {
+    
+    private ArrayList<NodoSimple<T>> nodos;
+
+    public SinglyLinkedList() {
+        nodos = new ArrayList<>();
+    }
+
+    @Override
+    public void addLast(T element) {
+        nodos.add(new NodoSimple<> (element));
+    }
+
+    @Override
+    public T removeLast() {
+        if (isEmpty()) return null;
+        return nodos.remove(nodos.size() - 1).getData();
+    }
+
+    @Override
+    public T getLast() {
+        if (isEmpty()) return null;
+        return nodos.get(nodos.size() - 1).getData();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return nodos.isEmpty();
+    }
+
+    @Override
+    public int size() {
+        return nodos.size();
+    }
+}
