cb10d7805ccaba1870dfce9640f0ca5a3a36f70f
starwald <mal25717@uvg.edu.gt>
2026-02-15 17:16:16 -0600

    Implementación de ArrayList para la pila

---
+++ StackArrayList.java
@@ -0,0 +1,37 @@
+package HojaDeTrabajo4;
+
+import java.util.ArrayList;
+
+public class StackArrayList<T> implements Stack<T> {
+    
+    private ArrayList<T> data = new ArrayList<>();
+
+    
+    @Override
+    public void push(T element) {
+        data.add(element);
+        System.out.println("Push (ArrayList): " + element);
+    }
+    
+    @Override
+    public T pop() {
+        if (isEmpty()) return null;
+        return data.remove(data.size() - 1);
+    }
+        
+    @Override
+    public T peek() {
+        if (isEmpty()) return null;
+        return data.get(data.size() - 1);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return data.isEmpty();
+    }
+
+    @Override
+    public int size() {
+        return data.size();
+    }
+}
